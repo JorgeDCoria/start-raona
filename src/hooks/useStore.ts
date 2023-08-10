877288ffb57fa5bfb97f274150b9a15835adf1bc
Jorge Coria <coriajorge.dev.apu@gmail.com>
2023-08-09 23:34:55 -0300

    perf/ estado global, se definen tipos y hook para manejar la inferencia de stado y action

---
+++ src/hooks/useStore.ts
@@ -0,0 +1,6 @@
+import { AppDispatch, RootState } from "../redux/store/store";
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;
