950082fdccc63adfc3016d9e0a0a1ade26bf8f75
Jorge Coria <coriajorge.dev.apu@gmail.com>
2023-08-11 14:42:18 -0300

    perf/ searchBar, se implementa la busqueda por titulo de pelicula

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,17 +1,34 @@
 import { PrimaryButton } from "@fluentui/react";
-import React from "react";
 import { RiSearch2Line } from "react-icons/ri";
 import "../../App.css";
+import { useState } from "react";
+import { useAppDispatch } from "../../hooks/useStore";
+import { actionGetMoviesByTitle } from "../../redux/actions/moviesActions";
 const SearchBar = () => {
+  const [input, setInput] = useState<string>("");
+  const dispatch = useAppDispatch();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(actionGetMoviesByTitle(input));
+    setInput("");
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
   return (
-    <div className="search-bar">
+    <form className="search-bar" onSubmit={handleSubmit}>
       <div className="search-input">
-        <input type="text" />
+        <input type="text" value={input} name="input" onChange={handleChange} />
 
         <RiSearch2Line className="icon-search-bar" />
       </div>
-      <PrimaryButton className="button-primary" text="Buscar" />
-    </div>
+      <PrimaryButton
+        className="button-primary"
+        text="Buscar"
+        type="submit
+      "
+      />
+    </form>
   );
 };
 
