763dad0088806087dd0762f5f61bffdb0a6fe837
Jorge Coria <coriajorge.dev.apu@gmail.com>
2023-08-12 05:53:36 -0300

    feat/ NotFound component

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,5 +1,17 @@
-const NotFound = () => {
-  return <div>NotFound</div>;
+import Footer from "../../components/layout/footer/Footer";
+import NavBar from "../../components/layout/navbar/NavBar";
+
+const NotFound: React.FC = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="not-found">
+        <h1>404</h1>
+        <p>¡Página no encontrada!</p>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default NotFound;
